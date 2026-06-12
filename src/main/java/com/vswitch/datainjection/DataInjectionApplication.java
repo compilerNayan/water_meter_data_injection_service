774787ae30a5e9edaa10a93ad969543bd094773b
Nayan <geeknayan@gmail.com>
2026-06-12 13:01:00 +0530

    khjdasjdlkas

---
+++ src/main/java/com/vswitch/datainjection/DataInjectionApplication.java
@@ -0,0 +1,12 @@
+package com.vswitch.datainjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataInjectionApplication.class, args);
+    }
+}
